a7a2620042b857c0709d5d53e528d9dea70a821b
XiaoA <awinds@qq.com>
2024-09-09 22:06:58 +0800

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,8 +8,8 @@ import { GoogleAnalytics } from '@next/third-parties/google'
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "图床",
-  description: "图床",
+  title: "Pic.Pub",
+  description: "Pic.Pub图床",
 };
 
 
